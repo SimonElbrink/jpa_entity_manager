f7ba3aeb9b8b4cb88a041d96581dff188ecb213c
Simon Elbrink <Simon.Elbrink@gmail.com>
2019-11-04 13:18:52 +0100

    Created a Dao
    FindByName and FindByEmail coming next.

---
+++ src/main/java/se/lexicon/simon/jpa_entity_manager/database/AppUserDaoImpl.java
@@ -0,0 +1,64 @@
+package se.lexicon.simon.jpa_entity_manager.database;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import se.lexicon.simon.jpa_entity_manager.entity.AppUser;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class AppUserDaoImpl {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+
+    @Transactional
+    public List<AppUser> findAll(){
+        List<AppUser> result = new ArrayList<>();
+        TypedQuery<AppUser> query =
+                entityManager.createQuery("SELECT u FROM AppUser u", AppUser.class);
+
+        result = query.getResultList();
+        return result;
+    }
+
+    @Transactional
+    public Optional<AppUser> findById(int id){
+        return Optional.ofNullable(entityManager.find(AppUser.class, id));
+    }
+
+    @Transactional(rollbackFor = RuntimeException.class)
+    public AppUser save (AppUser appUser){
+
+        if(appUser.getId() != 0){
+            update(appUser);
+        }
+
+       entityManager.persist(appUser);
+       return appUser;
+    }
+
+    @Transactional(rollbackFor = RuntimeException.class)
+    public void delete (int id){
+        Optional<AppUser> result = findById(id);
+
+        entityManager.remove(result.orElseThrow(IllegalArgumentException::new));
+    }
+
+    @Transactional(rollbackFor = RuntimeException.class)
+    public AppUser update (AppUser appUser){
+
+        if(appUser.getId() == 0){
+            return save(appUser);
+        }
+
+        return entityManager.merge(appUser);
+    }
+
+}
