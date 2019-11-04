1099184cd89620a6f8f5b5e30bb24b429cd991f0
Simon Elbrink <Simon.Elbrink@gmail.com>
2019-11-04 13:16:40 +0100

    Initial commit

---
+++ src/test/java/se/lexicon/simon/jpa_entity_manager/JpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.simon.jpa_entity_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
